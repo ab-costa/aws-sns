ae125bcc3a3b0e32e1ee19e10ef0b8001b28bf79
ab-costa <costa.alessandroborges@gmail.com>
2022-05-01 17:30:47 -0300

    Feature: Criação do método sendMessage na classe SNSService

--- src/main/java/services/SNSService.java
+++ src/main/java/services/SNSService.java
@@ -1,5 +1,32 @@
 package services;
 
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+
 public class SNSService {
-    
+
+    private static String topicArn = "";
+
+    public static void sendMessage(String message) {
+
+        AwsCredentialsProvider credentialsProvider = new AwsCredentialsProvider() {
+
+            @Override
+            public AwsCredentials resolveCredentials() {
+
+                return new AwsCredentials() {
+
+                    @Override
+                    public String accessKeyId() {
+                        return System.getenv("AWS_ACCESS_KEY");
+                    }
+
+                    @Override
+                    public String secretAccessKey() {
+                        return System.getenv("AWS_SECRET_KEY");
+                    }                    
+                };
+            }            
+        };
+    }
 }
